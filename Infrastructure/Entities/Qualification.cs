0f2aa039efb25960396b33f16378701615a4d4cb
Thái Chí Bảo <comaybayy@gmail.com>
2022-09-18 23:02:10 +0700

    add domain entities

---
+++ Infrastructure/Entities/Qualification.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary_management.Infrastructure.Entities
+{
+	public class Qualification
+	{
+		public int Id { get; set; }
+		
+		[MaxLength(255)] 
+		public string Name { get; set; } = null!;
+		
+		public DateOnly IssueDate { get; set; }
+		
+		[MaxLength(255)] 
+		public string PlaceOfIssue { get; set; } = null!;
+		
+		public int Allowance { get; set; } 
+
+		public int ExpertiseId { get; set; }
+		public Expertise Expertise { get; set; } = null!;
+
+		public string StaffId { get; set; } = null!;
+		public Staff Staff { get; set; } = null!;
+
+	}
+}
