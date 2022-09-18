afbe9194863071d2836d2cdd046479629ba70cf8
Thái Chí Bảo <comaybayy@gmail.com>
2022-09-18 21:38:11 +0700

    setup EFCore with Postgresql, basic dbcontext

---
+++ Infrastructure/SalaryManContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary_management.Infrastructure
+{
+	public class SalaryManContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+			=> optionsBuilder.UseNpgsql("Host=localhost;Database=SalaryMan;Username=postgres;Password=postgres")
+							 .UseSnakeCaseNamingConvention();
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			//TODO: seeding some shit
+		}
+	}
+}
