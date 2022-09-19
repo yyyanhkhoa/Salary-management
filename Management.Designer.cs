3bb1692eaf4537d2ef0b48b977c35efd3588ae62
khanhsd0901 <khanhsd0901@gmail.com>
2022-09-19 20:16:11 +0700

    effect cho giao dien de de su dung

--- Management.Designer.cs
+++ Management.Designer.cs
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Management));
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.SettingBtn = new System.Windows.Forms.Button();
+            this.PeronalBtn = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.panelTitleBar = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.panelMenu.Controls.Add(this.SettingBtn);
+            this.panelMenu.Controls.Add(this.PeronalBtn);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(220, 616);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // SettingBtn
+            // 
+            this.SettingBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.SettingBtn.FlatAppearance.BorderSize = 0;
+            this.SettingBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SettingBtn.ForeColor = System.Drawing.Color.Gainsboro;
+            this.SettingBtn.Image = ((System.Drawing.Image)(resources.GetObject("SettingBtn.Image")));
+            this.SettingBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.SettingBtn.Location = new System.Drawing.Point(0, 146);
+            this.SettingBtn.Name = "SettingBtn";
+            this.SettingBtn.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.SettingBtn.Size = new System.Drawing.Size(220, 66);
+            this.SettingBtn.TabIndex = 2;
+            this.SettingBtn.Text = "    Setting";
+            this.SettingBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.SettingBtn.UseVisualStyleBackColor = true;
+            this.SettingBtn.Click += new System.EventHandler(this.SettingBtn_Click);
+            // 
+            // PeronalBtn
+            // 
+            this.PeronalBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PeronalBtn.FlatAppearance.BorderSize = 0;
+            this.PeronalBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PeronalBtn.ForeColor = System.Drawing.Color.Gainsboro;
+            this.PeronalBtn.Image = ((System.Drawing.Image)(resources.GetObject("PeronalBtn.Image")));
+            this.PeronalBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.PeronalBtn.Location = new System.Drawing.Point(0, 80);
+            this.PeronalBtn.Name = "PeronalBtn";
+            this.PeronalBtn.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.PeronalBtn.Size = new System.Drawing.Size(220, 66);
+            this.PeronalBtn.TabIndex = 1;
+            this.PeronalBtn.Text = "    Personal";
+            this.PeronalBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.PeronalBtn.UseVisualStyleBackColor = true;
+            this.PeronalBtn.Click += new System.EventHandler(this.PeronalBtn_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(220, 80);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitleBar.Location = new System.Drawing.Point(220, 0);
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.Size = new System.Drawing.Size(763, 80);
+            this.panelTitleBar.TabIndex = 1;
+            // 
+            // Management
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(983, 616);
+            this.Controls.Add(this.panelTitleBar);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "Management";
             this.Text = "Management";
+            this.panelMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Panel panelMenu;
+        private Button PeronalBtn;
+        private Panel panelLogo;
+        private Button SettingBtn;
+        private Panel panelTitleBar;
     }
 }
--- Management.cs
+++ Management.cs
@@ -12,9 +12,70 @@ namespace Salary_management
 {
     public partial class Management : Form
     {
+        private Button currentButton;
+        private Random random;
+        private int tempIndex;
+
         public Management()
         {
             InitializeComponent();
+            random = new Random();
+        }
+
+        private Color SelectThemeColor()
+        {
+            int index = random.Next(ThemeColor.ColorList.Count);
+            while (tempIndex == index)
+            {
+                index = random.Next(ThemeColor.ColorList.Count);
+            }
+            tempIndex = index;
+            string color = ThemeColor.ColorList[index];
+            return ColorTranslator.FromHtml(color);
+        }
+
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    Color color = SelectThemeColor();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    panelTitleBar.BackColor = color;
+                    panelLogo.BackColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                    ThemeColor.PrimaryColor = color;
+                    ThemeColor.SecondaryColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                }
+            }
+        }
+
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panelMenu.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(51, 51, 76);
+                    previousBtn.ForeColor = Color.Gainsboro;
+                    previousBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+
+        private void PeronalBtn_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void SettingBtn_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+
         }
     }
 }
