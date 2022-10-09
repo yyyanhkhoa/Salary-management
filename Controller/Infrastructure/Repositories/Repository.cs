9399ca10cb7f3de127b9ea53abc55fc631caa03f
Thái Chí Bảo <bao.thai@spiraledge.com>
2022-10-09 12:07:21 +0700

    rename namespaces and add Data folder

--- Controller/Infrastructure/Repositories/Repository.cs
+++ Controller/Infrastructure/Repositories/Repository.cs
@@ -1,4 +1,10 @@
 ﻿using Salary_management.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Salary_management.Controller.Infrastructure.Repositories
 {
 	public abstract class Repository
