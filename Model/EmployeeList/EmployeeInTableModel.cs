3990bd9cad0d0ba4275ea95195af2f096fb06925
khanhsd0901 <khanhsd0901@gmail.com>
2022-11-01 09:21:06 +0700

    add view employee

---
+++ Model/EmployeeList/EmployeeInTableModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary_management.Model.EmployeeList
+{
+    internal class EmployeeInTableModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public DateOnly Birth { get; set; }
+
+        public string Identity { get; set; }
+
+        public float Allowance { get; set; }
+
+        public EmployeeInTableModel(string id, string name, DateOnly birth, string identity, float allowance)
+        {
+            Id = id;
+            Name = name;
+            Birth = birth;
+            Identity = identity;
+            Allowance = allowance;
+        }
+    }
+}
