33eb36470a6f95743da725b3b491028b510b0889
khanhsd0901 <khanhsd0901@gmail.com>
2022-11-05 09:35:15 +0700

    add rank

--- View/Forms/Position/AddPosition.cs
+++ View/Forms/Position/AddPosition.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Salary_management.Controller.Infrastructure.Data.Input;
+using Salary_management.Controller.Infrastructure.Repositories;
+using Salary_management.Infrastructure.Entities.Enums;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -22,6 +25,42 @@ namespace Salary_management.View.Forms.Position
         private void BackBtn_Click(object sender, EventArgs e)
         {
             mng.OpenChildForm(new View.Forms.Position.ListPosition(this.mng), sender);
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            var RepoEmployee = new RepositoryEmployee();
+            
+            /*
+            var RepoEmployee = new RepositoryEmployee();
+            Gender gender;
+            if (MaleBtn.Checked) gender = Gender.Male;
+            else gender = Gender.Female;
+            DateOnly dateOfBirth = DateOnly.FromDateTime(DateOfBirth.Value);
+            DateOnly startDate = DateOnly.FromDateTime(StartDate.Value);
+            //MessageBox.Show(name + " " + gender + " " + dateOfBirth + " " + ethnic + " " + address + " " + startDate + " " + identity + " " + coefficientAllowance);
+            var result = RepoEmployee.InsertEmployee(new EmployeeInput()
+            {
+                Name = NameText.Text,
+                Gender = gender,
+                DateOfBirth = dateOfBirth,
+                Ethnic = EthnicText.Text,
+                Address = AddressText.Text,
+                StartDate = startDate,
+                IdentityCardNumber = IdentityText.Text,
+                CoefficientAllowance = float.Parse(CoefficientAllowanceText.Text),
+            });
+            if (result.Success)
+            {
+                MessageBox.Show("Insert Employee success");
+                mng.OpenChildForm(new View.Forms.Employee.ListInformation(this.mng), sender);
+            }
+            else
+            {
+                MessageBox.Show(result.ErrorMessage);
+            }*/
+
+
         }
     }
 }
