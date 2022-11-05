7755c57d29ebad83f1d8059031850a3128335a08
Thái Chí Bảo <bao.thai@spiraledge.com>
2022-11-05 11:48:54 +0700

    employee detail logic and updated employee logic

--- Model/DetailInformation/EmployeeDetail.cs
+++ Model/DetailInformation/EmployeeDetail.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Salary_management.Infrastructure.Entities.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Salary_management.Model
 {
-	public class EmployeeDetail : Infrastructure.Entities.Employee
+	public class EmployeeDetail
 	{
+		public string Id { get; set; } = null!;
+		public string Name { get; set; } = null!;
+		public Gender Gender { get; set; }
+		public DateOnly DateOfBirth { get; set; }
+		public string Ethnic { get; set; } = null!;
+		public DateOnly StartDate { get; set; }
+		public string Address { get; set; } = null!;
+		public string IdentityCardNumber { get; set; } = null!;
+		public byte[]? Image { get; set; }
+		public float CoefficientAllowance { get; set; }
+		public DateTime DateCreated { get; set; }
 	}
 }
--- Model/Employee.cs
+++ Model/Employee.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Salary_management.Infrastructure.Entities.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Salary_management.Model
 {
-	public class Employee : Infrastructure.Entities.Employee
+	public class Employee 
 	{
+		public string Id { get; set; } = null!;
+
+		public string Name { get; set; } = null!;
+
+		public DateOnly DateOfBirth { get; set; }
+
+		public string IdentityCardNumber { get; set; } = null!;
+
+		public float CoefficientAllowance { get; set; }
 	}
 }
