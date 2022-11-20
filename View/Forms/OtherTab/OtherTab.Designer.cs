ebafa1166bc89797cc09db905d28d22640f5a544
khanhsd0901 <khanhsd0901@gmail.com>
2022-11-20 12:25:14 +0700

    add union in unit

--- View/Forms/OtherTab/OtherTab.Designer.cs
+++ View/Forms/OtherTab/OtherTab.Designer.cs
@@ -39,8 +39,11 @@
             this.panel4 = new System.Windows.Forms.Panel();
             this.DeleteBtn = new System.Windows.Forms.Button();
             this.AddBtn = new System.Windows.Forms.Button();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
             this.NameText = new System.Windows.Forms.TextBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.IDText = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.NextBtn = new System.Windows.Forms.Button();
@@ -51,6 +54,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.ListUnionGrid)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.panel3.SuspendLayout();
+            this.panel5.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -121,6 +125,7 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.panel3);
+            this.groupBox1.Controls.Add(this.panel5);
             this.groupBox1.Controls.Add(this.panel2);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
             this.groupBox1.Location = new System.Drawing.Point(3, 55);
@@ -136,11 +141,11 @@
             this.panel3.Controls.Add(this.DeleteBtn);
             this.panel3.Controls.Add(this.AddBtn);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(3, 106);
+            this.panel3.Location = new System.Drawing.Point(3, 189);
             this.panel3.Name = "panel3";
             this.panel3.Padding = new System.Windows.Forms.Padding(10);
             this.panel3.Size = new System.Drawing.Size(282, 63);
-            this.panel3.TabIndex = 1;
+            this.panel3.TabIndex = 3;
             // 
             // panel4
             // 
@@ -171,16 +176,16 @@
             this.AddBtn.UseVisualStyleBackColor = true;
             this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
             // 
-            // panel2
+            // panel5
             // 
-            this.panel2.Controls.Add(this.NameText);
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(3, 23);
-            this.panel2.Name = "panel2";
-            this.panel2.Padding = new System.Windows.Forms.Padding(20);
-            this.panel2.Size = new System.Drawing.Size(282, 83);
-            this.panel2.TabIndex = 0;
+            this.panel5.Controls.Add(this.NameText);
+            this.panel5.Controls.Add(this.NameLabel);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(3, 106);
+            this.panel5.Name = "panel5";
+            this.panel5.Padding = new System.Windows.Forms.Padding(20);
+            this.panel5.Size = new System.Drawing.Size(282, 83);
+            this.panel5.TabIndex = 2;
             // 
             // NameText
             // 
@@ -190,15 +195,44 @@
             this.NameText.Size = new System.Drawing.Size(186, 27);
             this.NameText.TabIndex = 1;
             // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.NameLabel.Location = new System.Drawing.Point(20, 20);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(56, 20);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Name :";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.IDText);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(3, 23);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(20);
+            this.panel2.Size = new System.Drawing.Size(282, 83);
+            this.panel2.TabIndex = 0;
+            // 
+            // IDText
+            // 
+            this.IDText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.IDText.Location = new System.Drawing.Point(51, 20);
+            this.IDText.Name = "IDText";
+            this.IDText.Size = new System.Drawing.Size(211, 27);
+            this.IDText.TabIndex = 1;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Dock = System.Windows.Forms.DockStyle.Left;
             this.label1.Location = new System.Drawing.Point(20, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 20);
+            this.label1.Size = new System.Drawing.Size(31, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Name :";
+            this.label1.Text = "ID: ";
             // 
             // panel1
             // 
@@ -219,6 +253,7 @@
             this.NextBtn.TabIndex = 1;
             this.NextBtn.Text = "Next >";
             this.NextBtn.UseVisualStyleBackColor = true;
+            this.NextBtn.Click += new System.EventHandler(this.NextBtn_Click);
             // 
             // QualificationTab
             // 
@@ -245,6 +280,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.ListUnionGrid)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.panel1.ResumeLayout(false);
@@ -264,12 +301,15 @@
         private DataGridViewTextBoxColumn IDGrid;
         private DataGridViewTextBoxColumn NameUnionGrid;
         private GroupBox groupBox1;
+        private Panel panel2;
+        private TextBox IDText;
+        private Label label1;
         private Panel panel3;
         private Panel panel4;
         private Button DeleteBtn;
         private Button AddBtn;
-        private Panel panel2;
+        private Panel panel5;
         private TextBox NameText;
-        private Label label1;
+        private Label NameLabel;
     }
 }
