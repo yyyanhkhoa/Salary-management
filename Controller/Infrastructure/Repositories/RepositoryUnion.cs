46fed2a49ea52f8919c0b5f73fdb28da2972fded
comaybay <comaybayy@gmail.com>
2022-11-20 10:55:04 +0700

    add UnionRepository

--- Controller/Infrastructure/Repositories/RepositoryUnion.cs
+++ Controller/Infrastructure/Repositories/RepositoryUnion.cs
@@ -16,12 +16,12 @@ namespace Salary_management.Controller.Infrastructure.Repositories
 		public bool CheckUnionExist(string name)
 			=> Context.Unions.Any(a => a.Name == name);
 
-		public Result<Models.Union> InsertUnion(string name)
+		public Result<Models.Union> InsertUnion(string id, string name)
 		{
 			if (CheckUnionExist(name))
 				return new Result<Models.Union> { Success = false, ErrorMessage = "Union with this name already exists." };
 
-			var union = new Models.Union { Name = name };
+			var union = new Models.Union { Name = name , Id = id};
 			Context.Unions.Add(union);
 			Context.SaveChanges();
 			return new Result<Models.Union> { Success = true, Payload = union };
@@ -68,6 +68,32 @@ namespace Salary_management.Controller.Infrastructure.Repositories
 			Context.Unions.Attach(union);
 			Context.Unions.Remove(union);
 			Context.SaveChanges();
+		}
+
+		public Result<List<Models.UnionTimeline>> GetTimeline(string unionId, DateOnly? from = null, DateOnly? to = null)
+		{
+			var query = Context.UnionHistories.Where(uh => uh.UnionId == unionId);
+			query = Helper.GetTimelineByDate(query, from, to);
+
+			return new()
+			{
+				Success = true,
+				Payload = query.Include(uh => uh.Employee)
+							   .Select(ph => MapToTimelineModel(ph))
+							   .ToList()
+			};
+		}
+
+		private Models.UnionTimeline MapToTimelineModel(UnionHistory entity)
+		{
+			return new Models.UnionTimeline
+			{
+				EmployeeName = entity.Employee.Name,
+				UnionId = entity.UnionId,
+				StartDate = entity.StartDate,
+				EndDate = entity.EndDate,
+				EmployeeId = entity.EmployeeId
+			};
 		}
 
 		private static Models.Union MapToModel(Union entity)
---
+++ Model/UnionTimeline.cs
@@ -0,0 +1,15 @@
+﻿using Salary_management.Controller.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary_management.Model
+{
+	public class UnionTimeline : History
+	{
+		public string EmployeeName { get; set; } = null!;
+		public string UnionId { get; set; } = null!;
+	}
+}
