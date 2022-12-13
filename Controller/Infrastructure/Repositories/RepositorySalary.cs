a7e67444609418bb5b63f41db95ae5bbce9d5b62
comaybay <comaybayy@gmail.com>
2022-12-13 21:03:59 +0700

    Get All Salaries

--- Controller/Infrastructure/Repositories/RepositorySalary.cs
+++ Controller/Infrastructure/Repositories/RepositorySalary.cs
@@ -11,6 +11,71 @@ namespace Salary_management.Controller.Infrastructure.Repositories
 {
 	public class RepositorySalary : Repository
 	{
+		public Result<List<Models.EmployeeSalary>> GetEmployeeSalaries(DateOnly? monthAndYear)
+		{
+			DateOnly date = monthAndYear ?? DateOnly.FromDateTime(DateTime.Now);
+
+			var endOfThisMonth = $"(make_date({date.Year}, {date.Month}, 1) + interval '1 month - 1 day')";
+			var totalCoefficient = $"(emp.coefficient_allowance + rank.coefficient + 0.33 * TRUNC(1 + ({date.Year} - EXTRACT(YEAR FROM emp.start_date)) / rank.milestone))";
+
+			var query = Context.EmployeeSalaries.FromSqlRaw(@$"
+				SELECT 
+					make_date({date.Year}, {date.Month}, 1) AS date,
+					emp.id AS employee_id,
+					emp.name AS employee_name,
+					unit.id AS unit_id,
+					unit.name AS unit_name,
+					TRUNC({totalCoefficient}::numeric, 2) AS total_coefficient,
+					emp.coefficient_allowance AS employee_allowance_coefficient,
+					pos.base_salary AS base_salary,
+					coalesce(total_qualification_allowance.total, 0) AS total_qualification_allowance,
+					TRUNC(1 + ({date.Year} - EXTRACT(YEAR FROM emp.start_date)) / rank.milestone) AS rank_level,
+					TRUNC(rank.coefficient::numeric, 2) AS rank_coefficient,
+					rank.name AS rank_name,
+					ROUND(coalesce(total_qualification_allowance.total, 0) + pos.base_salary * {totalCoefficient})::numeric AS salary 
+				FROM (
+					SELECT DISTINCT ON (employee_id) employee_id, unit_id, start_date, end_date
+					FROM unit_histories
+					WHERE start_date < {endOfThisMonth} 
+						AND (end_date IS NULL OR end_date >= {endOfThisMonth})
+				) AS uh 
+					INNER JOIN position_histories AS ph 
+						ON ph.employee_id = uh.employee_id 
+						AND ph.start_date < {endOfThisMonth}
+						AND (ph.end_date IS NULL OR ph.end_date >= {endOfThisMonth})
+					INNER JOIN employees AS emp 
+						ON emp.id = uh.employee_id
+					LEFT JOIN (
+						SELECT SUM(lastest_qua_allowance.allowance) AS total, emp.id AS employee_id
+						FROM qualifications AS qa
+							INNER JOIN employee_qualifications AS emp_qa 
+								ON qa.id = emp_qa.qualification_id
+							INNER JOIN employees AS emp 
+								ON emp.id = emp_qa.employee_id
+							INNER JOIN (
+								SELECT id,
+								(SELECT allowance FROM qualification_allowance_histories AS qah 
+									WHERE year <= {date.Year} AND qah.qualification_id = qualifications.id
+									ORDER BY year DESC
+									LIMIT 1
+								) AS allowance
+								FROM qualifications 
+							) AS lastest_qua_allowance
+								ON lastest_qua_allowance.id = qa.id
+						WHERE emp_qa.employee_id = emp.id
+						GROUP BY emp.id
+					) AS total_qualification_allowance
+						ON total_qualification_allowance.employee_id = emp.id
+					INNER JOIN units AS unit 
+						ON unit.id = uh.unit_id
+					INNER JOIN positions AS pos 
+						ON pos.id = ph.position_id
+					INNER JOIN ranks AS rank 
+						ON rank.id = pos.rank_id
+			");
+
+			return new() { Success = true, Payload = query.ToList() };
+		}
 		public Result<List<Models.EmployeeSalary>> GetEmployeeSalariesAtUnit(string unitId, DateOnly? monthAndYear)
 		{
 			DateOnly date = monthAndYear ?? DateOnly.FromDateTime(DateTime.Now);
--- Program.cs
+++ Program.cs
@@ -10,12 +10,13 @@ namespace Salary_management
         [STAThread]
         static void Main()
         {
+			//var resAll = new RepositorySalary().GetEmployeeSalaries(new DateOnly(2022, 12, 1));
 			//var resOmor = new RepositorySalary().GetEmployeeSalariesAtUnit("U_DAMN", new DateOnly(2022, 12, 1));
 			//var res = new RepositorySalary().GetEmployeeSalaryAtYear("EM_5", 2022);
-
+		
 			// To customize application configuration such as set high DPI settings or default font,
 			// see https://aka.ms/applicationconfiguration.
-			
+
 			ApplicationConfiguration.Initialize();
             Application.Run(new Management(Controller.Infrastructure.Entities.Enums.Role.Admin));
             
