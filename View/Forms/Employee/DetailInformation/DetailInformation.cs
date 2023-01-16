d3e9f60ccc4d9ce32e7e28fca5d57f580545c56a
yyyanhkhoa <80150282+yyyanhkhoa@users.noreply.github.com>
2023-01-16 12:20:52 +0700

    up

--- View/Forms/Employee/DetailInformation/DetailInformation.cs
+++ View/Forms/Employee/DetailInformation/DetailInformation.cs
@@ -183,16 +183,18 @@ namespace Salary_management.View.Forms.Employee.DetailInformation
             double[] xPositions = {1,2,3,4,5,6,7,8,9,10,11,12 };
             string[] xLabels = { "Jan", "Feb", "Mar", "Apr" ,"May","Jun" , "Jul", "Aug", "Sep","Oct", "Nov", "Dec" };            
             formsPlot1.Plot.XAxis.ManualTickPositions(xPositions, xLabels);
-            
-            setChart();
+
+            // du lieu mau
+            double[] dataX = { 1, 2, 3, 4, 5 };
+            double[] dataY = { 1, 4, 9, 16, 25 };
+            setChart(dataX, dataY);
 
         }
 
-        private void setChart()
+        private void setChart(double[] dataX, double[] dataY)
         {
             formsPlot1.Refresh();
-            double[] dataX = new double[] { 1, 2, 3, 4, 5 };
-            double[] dataY = new double[] { 1, 4, 9, 16, 25 };
+         
             formsPlot1.Plot.AddScatter(dataX, dataY);
             
         }
